09ff1c2b0ecef7f6c4caca94e1fc893d5a19ec3e
blackshady <victorblackshady@gmail.com>
2019-04-07 22:29:49 +0100

    setup docker

--- server/src/controllers/Meal.ts
+++ server/src/controllers/Meal.ts
@@ -1,50 +1,54 @@
-import {Request, Response} from 'express'
+import {Request, Response} from 'express';
 import MealServices from '../services/MealServices';
 import Filter from '../utils/Filter';
 
 class Meal {
-    private  mealService:any;
-    private filter:Filter;
+    private  mealService: any;
+    private filter: Filter;
 
-    constructor(){
+    constructor() {
        // This will be converted to use dependency injection
        this.mealService = new MealServices();
-       this.filter= new Filter();
+       this.filter = new Filter();
     }
 
-
-    public  getLeastIngredentId = async ( req:Request, res: Response) => {
+    /**
+     * getLeastIngredientId invoke the method to get ingredient
+     * @param req
+     * @param res
+     */
+    public  getLeastIngredientId = async (req: Request, res: Response) => {
         const {mealId} = req.params;
         const mealIdArr = mealId.split(',');
         const allMeals: any[] = [];
 
-        for(let id of mealIdArr){
+        for (const id of mealIdArr) {
             const response =  await this.mealService.getById(Number(id));
-            const {data, data:{meals}} = response;
-            if(meals === null){
-                return res.status(400).json({status: 'error',message: 'Bad request'})
+            const {data, data: {meals}} = response;
+            if (meals === null) {
+                return res.status(400).json({status: 'error', message: 'Bad request'});
             }
             allMeals.push( this.filter.mealSterilization(data));
         }
-       const leastMealId = this.filter.leastIngredentId(allMeals);
+        const leastMealId = this.filter.leastIngredentId(allMeals);
 
         return res.status(200).json({
             status:'success',
             message: 'Meal Id with least ingredient',
             mealId: leastMealId
          });
-    };
+    }
 
-    public getRandomId = async  (_:Request, res: Response) => {
+    public getRandomId = async  (_: Request, res: Response) => {
        const response = await this.mealService.getRandomMeal();
        const {data} = response;
-       const mealId = this.filter.mealIdSterilization(data)
+       const mealId = this.filter.mealIdSterilization(data);
        return res.status(200).json({
-           status:'success',
+           status: 'success',
            message: 'Random meal id',
            mealId,
-       })
-    };
+       });
+    }
 
 }
 
--- server/src/middlewares/MealValidation.ts
+++ server/src/middlewares/MealValidation.ts
@@ -12,7 +12,6 @@ class MealValidation{
             });
     }
 
-
 }
 
 export default MealValidation;
